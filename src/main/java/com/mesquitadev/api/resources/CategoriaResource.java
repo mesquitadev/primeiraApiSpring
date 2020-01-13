3b71130681c8a09e14f973e5d3e511f9fd0dbe05
Victor Mesquita <mesquitadev@gmail.com>
2020-01-13 00:13:49 -0300

    Add CategoryDAO

--- src/main/java/com/mesquitadev/api/resources/CategoriaResource.java
+++ src/main/java/com/mesquitadev/api/resources/CategoriaResource.java
@@ -1,14 +1,25 @@
 package com.mesquitadev.api.resources;
 
+import com.mesquitadev.api.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "REST Funcionando!";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Dev");
+        Categoria cat2 = new Categoria(2, "Design");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
